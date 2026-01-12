f2f42be9f706c95155694efc1b4f0aee5356a574
Salsabillah Rimadany <2511500084@mahasiswa.atmaluhur.ac.id>
2026-01-12 11:38:37 +0700

    mengubah menjadi read new.php

--- pertemuan-15/index.php
+++ pertemuan-15/index.php
@@ -165,7 +165,7 @@ require_once __DIR__ . '/fungsi.php';
       <br>
       <hr>
       <h2>Yang menghubungi kami</h2>
-      <?php include 'read new.php'; ?>
+      <?php include 'read_inc.php'; ?>
     </section>
   </main>
 
